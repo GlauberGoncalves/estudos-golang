0b72528d6cb1b261b9fc3bd7a4fa12a167ae8537
GlauberGoncalves <sistemas.glauber@gmail.com>
2019-11-19 23:58:05 -0300

    trabalhando com retorno nomeado em funções

---
+++ funcoes/retornonomeado/nomeado.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func getNome() (nome, sobrenome string) {
+	nome = "Glauber"
+	sobrenome = "Gonçalves"
+	return
+}
+
+func main() {
+	nome, sobrenome := getNome()
+
+	fmt.Println(nome, sobrenome)
+}
